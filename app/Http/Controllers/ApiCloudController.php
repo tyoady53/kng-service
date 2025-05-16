9797d94f5542b4ea4627b1de97f90166cafbe5de
tyoady53 <adyprasetyo.0103@gmail.com>
2025-05-16 19:35:23 +0700

    update

--- app/Http/Controllers/ApiCloudController.php
+++ app/Http/Controllers/ApiCloudController.php
@@ -37,7 +37,11 @@ class ApiCloudController extends Controller
                     $generated_id = $get->generated_id;
                     $detail = KendaraanDetail::where('id_kendaraan', $generated_id)->latest()->first();
                     if($detail && $detail->jenis != $data['jenis']) {
-                        $this->change_jenis($data);
+                        $ret = $this->change_jenis($data,$generated_id);
+
+                        if($ret) {
+                            return $ret;
+                        }
                     }
                 } else {
                     $milliseconds = round(microtime(true) * 1000);
@@ -125,7 +129,74 @@ class ApiCloudController extends Controller
         }
     }
 
-    function change_jenis($data) {
+    function change_jenis($data,$generated_id) {
+        $insert = KendaraanDetail::create([
+            'id_kendaraan'      => $generated_id,
+            'merek'             => $data['merek'],
+            'tipe'              => $data['tipe'],
+            'jenis'             => $data['jenis'],
+            'thpembuatan'       => $data['thpembuatan'],
+            'bahanbakar'        => $data['bahanbakar'],
+            'isisilinder'       => $data['isisilinder'],
+            'dayamotorpenggerak'=> $data['dayamotorpenggerak'],
+            'jbb'               => $data['jbb'],
+            'jbkb'              => $data['jbkb'],
+            'jbi'               => $data['jbi'],
+            'jbki'              => $data['jbki'],
+            'mst'               => $data['mst'],
+            'beratkosong'       => $data['beratkosong'],
+            'konfigurasisumburoda'  => $data['konfigurasisumburoda'],
+            'ukuranban'         => $data['ukuranban'],
+            'panjangkendaraan'  => $data['panjangkendaraan'],
+            'lebarkendaraan'    => $data['lebarkendaraan'],
+            'tinggikendaraan'   => $data['tinggikendaraan'],
+            'panjangbakatautangki'  => $data['panjangbakatautangki'],
+            'lebarbakatautangki'=> $data['lebarbakatautangki'],
+            'tinggibakatautangki'   => $data['tinggibakatautangki'],
+            'julurdepan'        => $data['julurdepan'],
+            'julurbelakang'     => $data['julurbelakang'],
+            'jaraksumbu1_2'     => $data['jaraksumbu1_2'],
+            'jaraksumbu2_3'     => $data['jaraksumbu2_3'],
+            'jaraksumbu3_4'     => $data['jaraksumbu3_4'],
+            'dayaangkutorang'   => $data['dayaangkutorang'],
+            'dayaangkutbarang'  => $data['dayaangkutbarang'],
+            'kelasjalanterendah'=> $data['kelasjalanterendah']
+        ]);
 
+        $masaberlakuuji = DateTime::createFromFormat('dmY', $data['masaberlakuuji']);
+        $tgl_uji = DateTime::createFromFormat('dmY', $data['tgluji']);
+        HasilUji::create([
+            'id_kendaraan'          => $generated_id,
+            'fotodepan'             => $data['fotodepansmall'],
+            'fotobelakang'          => $data['fotobelakangsmall'],
+            'fotokanan'             => $data['fotokanansmall'],
+            'fotokiri'              => $data['fotokirismall'],
+            'emisiasap'             => $data['alatuji_emisiasapbahanbakarsolar'],
+            'emisico'               => $data['alatuji_emisicobahanbakarbensin'],
+            'emisihc'               => $data['alatuji_emisihcbahanbakarbensin'],
+            'totalgayapengereman'   => $data['alatuji_remutamatotalgayapengereman'],
+            'selisihgayapengereman1'=> $data['alatuji_remutamaselisihgayapengeremanrodakirikanan1'],
+            'selisihgayapengereman2'=> $data['alatuji_remutamaselisihgayapengeremanrodakirikanan2'],
+            'selisihgayapengereman3'=> $data['alatuji_remutamaselisihgayapengeremanrodakirikanan3'],
+            'selisihgayapengereman4'=> $data['alatuji_remutamaselisihgayapengeremanrodakirikanan4'],
+            'remparkirtangan'       => $data['alatuji_remparkirtangan'],
+            'remparkirkaki'         => $data['alatuji_remparkirkaki'],
+            'kincuprodadepan'       => $data['alatuji_kincuprodadepan'],
+            'tingkatkebisingan'     => $data['alatuji_tingkatkebisingan'],
+            'kekuatanpancarlampukanan'  => $data['alatuji_lampuutamakekuatanpancarlampukanan'],
+            'kekuatanpancarlampukiri'   => $data['alatuji_lampuutamakekuatanpancarlampukiri'],
+            'penyimpanganlampukanan'=> $data['alatuji_lampuutamapenyimpanganlampukanan'],
+            'penyimpanganlampukiri' => $data['alatuji_lampuutamapenyimpanganlampukiri'],
+            'penunjukkecepatan'     => $data['alatuji_penunjukkecepatan'],
+            'kedalamanalurban'      => $data['alatuji_kedalamanalurban'],
+            'masaberlakuuji'        => $masaberlakuuji->format('Y-m-d'),
+            'tgl_uji'               => $tgl_uji->format('Y-m-d'),
+        ]);
+
+        if($insert) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
