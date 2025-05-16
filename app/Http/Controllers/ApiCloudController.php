649c3f03a45c57c9e8b3b77a4229758810cca25c
tyoady53 <adyprasetyo.0103@gmail.com>
2025-05-16 17:07:29 +0700

    update

--- app/Http/Controllers/ApiCloudController.php
+++ app/Http/Controllers/ApiCloudController.php
@@ -27,7 +27,7 @@ class ApiCloudController extends Controller
 
         $datas = $request->data;
 
-        return $datas[0];
+        return $datas[0]['nouji'];
         // dd($datas);
         $get = Kendaraan::where('no_uji', $datas[0]->nouji)->first();
         return $get;
